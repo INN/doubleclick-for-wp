beb17beba290cff846d1652097402212fbde9967
Will Haynes <haynes24@gmail.com>
2015-05-26 17:53:35 -0500

    release framework

--- dfw-init.php
+++ dfw-init.php
@@ -1,11 +1,4 @@
 <?php
-/*
-Plugin Name: 	DoubleClick for WordPress
-Description: 	A simple way to serve DoubleClick ads in WordPress.
-Version: 		0.1
-Author: 		Will Haynes for INN
-Author URI: 	http://github.com/inn
-*/
 
 /**
  * Global instances for DoubleClick object.
---
+++ dfw.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: 	DoubleClick for WordPress
+Description: 	A simple way to serve DoubleClick ads in WordPress.
+Version: 		0.1
+Author: 		Will Haynes for INN
+Author URI: 	http://github.com/inn
+*/
+
+include('dfw-init.php');
