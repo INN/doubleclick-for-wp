8e8ac90b27a4cb0a96914c7cbfed8e1bc445aa15
aschweigert <aschweigert@gmail.com>
2017-06-21 12:54:14 -0400

    address issues with dfw-options.php

--- dfw-options.php
+++ dfw-options.php
@@ -24,7 +24,7 @@ add_action( 'admin_menu', 'dfw_plugin_menu' );
 function dfw_option_page_html() {
 	// Nice try.
 	if ( ! current_user_can( 'manage_options' ) ) {
-		wp_die( __( 'You do not have sufficient permissions to access this page.', 'dfw' ) );
+		wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'dfw' ) );
 	}
 
 	echo '<div class="wrap">';
@@ -51,7 +51,7 @@ function dfw_register_options() {
 		'Network Settings',
 		'dfw_settings_section_intro',
 		'doubleclick-for-wordpress'
-	); // ($id, $title, $callback, $page)
+	);
 
 	// Add a section for network option
 	add_settings_section(
@@ -59,7 +59,7 @@ function dfw_register_options() {
 		'Breakpoints',
 		'dfw_breakpoints_section_intro',
 		'doubleclick-for-wordpress'
-	); // ($id, $title, $callback, $page)
+	);
 
 	// Add a section for network option
 	add_settings_section(
@@ -67,7 +67,7 @@ function dfw_register_options() {
 		'Documentation',
 		'dfw_documentation_section_intro',
 		'doubleclick-for-wordpress'
-	); // ($id, $title, $callback, $page)
+	);
 
 	// Network Code
 	add_settings_field(
@@ -76,7 +76,7 @@ function dfw_register_options() {
 		'dfw_network_code_input',
 		'doubleclick-for-wordpress',
 		'dfw_network_options'
-	); // ($id, $title, $callback, $page, $section, $args)
+	);
 
 	// Breakpoints
 	add_settings_field(
@@ -85,7 +85,7 @@ function dfw_register_options() {
 		'dfw_breakpoints_input',
 		'doubleclick-for-wordpress',
 		'dfw_breakpoint_options'
-	); // ($id, $title, $callback, $page, $section, $args)
+	);
 
 	register_setting( 'doubleclick-for-wordpress', 'dfw_network_code' );
 	register_setting( 'doubleclick-for-wordpress', 'dfw_breakpoints', 'dfw_breakpoints_save' );
@@ -107,23 +107,23 @@ function dfw_documentation_section_intro() {
 }
 
 function dfw_network_code_input() {
-	global $DoubleClick;
+	global $doubleclick;
 
-	if ( isset( $DoubleClick->networkCode ) ) {
-		echo '<input value="' . $DoubleClick->networkCode . ' (set in theme)" type="text" class="regular-text" disabled/>';
+	if ( isset( $doubleclick->network_code ) ) {
+		echo '<input value="' . esc_attr( $doubleclick->network_code ) . ' (set in theme)" type="text" class="regular-text" disabled/>';
 	} else {
-		echo '<input name="dfw_network_code" id="dfw_network_code" type="text" value="' . get_option( 'dfw_network_code' ) . '" class="regular-text" />';
+		echo '<input name="dfw_network_code" id="dfw_network_code" type="text" value="' . esc_attr( get_option( 'dfw_network_code' ) ) . '" class="regular-text" />';
 	}
 }
 
 function dfw_breakpoints_input() {
-	global $DoubleClick;
+	global $doubleclick;
 
-	foreach ( $DoubleClick->breakpoints as $b ) {
-		if ( ! $b->option ) {
-			echo '<input value="' . $b->identifier . '" type="text" class="medium-text" disabled />';
-			echo '<label> min-width</label><input value="' . $b->minWidth . '" type="number" class="small-text" disabled />';
-			echo '<label> max-width</label><input value="' . $b->maxWidth . '" type="number" class="small-text" disabled /> (set in theme)<br/>';
+	foreach ( $doubleclick->breakpoints as $breakpoint ) {
+		if ( ! $bbreakpoint->option ) {
+			echo '<input value="' . esc_attr( $breakpoint->identifier ) . '" type="text" class="medium-text" disabled />';
+			echo '<label> min-width</label><input value="' . esc_attr( $breakpoint->min_width ) . '" type="number" class="small-text" disabled />';
+			echo '<label> max-width</label><input value="' . esc_attr( $breakpoint->max_width ) . '" type="number" class="small-text" disabled /> (set in theme)<br/>';
 		}
 	}
 
@@ -134,21 +134,21 @@ function dfw_breakpoints_input() {
 		$identifier = ( isset( $breakpoints[ $i ]['identifier'] ) )? $breakpoints[ $i ]['identifier'] : '';
 		$min_width = ( isset( $breakpoints[ $i ]['min-width'] ) )? $breakpoints[ $i ]['min-width'] : '';
 		$max_width = ( isset( $breakpoints[ $i ]['max-width'] ) )? $breakpoints[ $i ]['max-width'] : ''; ?>
-		<input value="<?php echo $identifier; ?>"
+		<input value="<?php echo esc_attr( $identifier ); ?>"
 				placeholder="Name"
 				name="dfw_breakpoints[]"
 				type="text"
 				class="medium-text" />
 		<label> min-width
 			<input
-				value="<?php echo $min_width; ?>" placeholder="0"
+				value="<?php echo esc_attr( $min_width ); ?>" placeholder="0"
 				name="dfw_breakpoints[]"
 				type="number"
 				class="small-text" />
 		</label>
 		<label> max-width
 			<input
-				value="<?php echo $max_width; ?>"
+				value="<?php echo esc_attr( $max_width ); ?>"
 				placeholder="9999"
 				name="dfw_breakpoints[]"
 				type="number"
